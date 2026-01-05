b13cbe7714a27fe8f2eb9633a9f3485cdf792aed
buck4437 <51261544+Buck4437@users.noreply.github.com>
2026-01-05 14:58:18 +0800

    Add new puzz

--- packages/puzzle-parser/src/index.ts
+++ packages/puzzle-parser/src/index.ts
@@ -1,6 +1,6 @@
 import { parse as parseAkari } from "./parsers/AkariParser.js";
 import { type Grid as AkariGrid } from "./types/Akari.js";
-import puzzle from "./demo-puzzles/akari1.json" with { type: "json" };
+import puzzle from "./demo-puzzles/akari2.json" with { type: "json" };
 
 function createEmptyGrid(): AkariGrid {
   return parseAkari(puzzle) 
