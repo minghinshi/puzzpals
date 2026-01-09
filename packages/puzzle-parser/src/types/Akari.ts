f71174b89c1997fd6ce9f5ba8a4587b18db7a0b2
buck4437 <51261544+Buck4437@users.noreply.github.com>
2026-01-09 15:08:45 +0800

    bro

--- packages/puzzle-parser/src/types/Akari.ts
+++ packages/puzzle-parser/src/types/Akari.ts
@@ -3,33 +3,33 @@ const NO_INPUT = 2;
 type Clue = number | null;
 
 type CellData = {
-    isBlack: boolean;
-    number: Clue;
-    input: number;
+  isBlack: boolean;
+  number: Clue;
+  input: number;
 }
 
 type Grid = {
-    rows: number;
-    cols: number;
-    cells: Cell[];
-    type: string;
+  rows: number;
+  cols: number;
+  cells: Cell[];
+  type: string;
 }
 
 class Cell {
-    isBlack = false;
-    number: number | null = null;
-    input: number = NO_INPUT;
+  isBlack = false;
+  number: number | null = null;
+  input: number = NO_INPUT;
 
-    setClue(number: Clue) {
-      this.isBlack = true;
-      this.number = number;
-    }
+  setClue(number: Clue) {
+    this.isBlack = true;
+    this.number = number;
+  }
 
-    setData(data: CellData) {
-      this.isBlack = data.isBlack;
-      this.number = data.number;
-      this.input = data.input;
-    }
+  setData(data: CellData) {
+    this.isBlack = data.isBlack;
+    this.number = data.number;
+    this.input = data.input;
+  }
 }
 
 export { NO_INPUT, type Clue, type CellData, type Grid, Cell };
