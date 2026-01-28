d8fb7a93c926774faf861a23a3eaa69590d8d187
minghinshi <mhshi9768@gmail.com>
2026-01-28 14:36:57 +0800

    Fix tests and add test for progress saving

--- puzzpals-server/src/__tests__/socket.test.ts
+++ puzzpals-server/src/__tests__/socket.test.ts
@@ -1,47 +1,75 @@
 import assert from "node:assert/strict";
-import { afterEach, beforeEach, describe, todo } from "node:test";
+import { afterEach, beforeEach, describe, it } from "node:test";
 import request from "supertest";
 
 import app from "src/app.js";
 import { arrangeBeforeEach, cleanUpAfterEach, } from "./utils/arrange.js";
 import { createMockSocket, mockBroadcast, mockIo } from "src/__mocks__/io.js";
+import { __resetForTests, startAutosave } from "src/memorystore.js";
+
+function assertEmit(actual: any[], expectedEvent: string, ...expectedPayload: any[]) {
+  assert.equal(actual[0], expectedEvent);
+  const actualPayload = actual.slice(1).map(x => JSON.parse(JSON.stringify(x)));
+  assert.deepEqual(actualPayload, expectedPayload);
+}
 
 describe("Socket", () => {
   beforeEach(arrangeBeforeEach);
   afterEach(cleanUpAfterEach);
 
+  const payload = {
+    type: "akari",
+    grid: [["."]],
+  };
+
+  const expectedGrid = {
+    rows: 1,
+    cols: 1,
+    cells: [
+      {
+        isBlack: false,
+        number: null,
+        input: 2,
+      },
+    ],
+    type: "akari",
+  };
+
   // As a player, I want to synchronise my progress with other players
   // so that we can collaborate on the same puzzle.
-  todo("synchronizes grid with all players in same room", async () => {
+  it("synchronizes grid with all players in same room", async () => {
     // Open room
-    const payload = {
-      type: "akari",
-      grid: [["."]],
-    };
-
     const res = await request(app).post("/api/rooms/create").send(payload);
     const token = res.body.token;
 
     const socket = createMockSocket();
     socket.call("room:join", { token });
 
-    const expectedGrid = {
-      rows: 1,
-      cols: 1,
-      cells: [
-        {
-          isBlack: false,
-          number: null,
-          input: 2,
-        },
-      ],
-      type: "akari",
-    };
-
-    assert.deepEqual(socket.emit.mock.calls[1]!.arguments, ["grid:state", expectedGrid]);
+    assertEmit(socket.emit.mock.calls[1]!.arguments, "grid:state", expectedGrid);
 
     socket.call("grid:updateCell", { token, idx: 0, value: 0 });
     assert.deepEqual(mockIo.to.mock.calls[0]!.arguments, [token]);
-    assert.deepEqual(mockBroadcast.mock.calls[0]!.arguments, ["grid:cellUpdated", { idx: 0, value: 0 }]);
+    assertEmit(mockBroadcast.mock.calls[0]!.arguments, "grid:cellUpdated", { idx: 0, value: 0 });
+  });
+
+  it("restores room progress after server shuts down", async (t) => {
+    t.mock.timers.enable({ apis: ["setInterval"] });
+
+    // Enable autosave in this test to allow the timer to call autosave
+    startAutosave();
+
+    const res = await request(app).post("/api/rooms/create").send(payload);
+    const token = res.body.token;
+
+    // Wait for 1 minute
+    t.mock.timers.tick(60 * 1000);
+
+    // "Shut down" the server, wiping memory
+    __resetForTests();
+
+    const socket = createMockSocket();
+    socket.call("room:join", { token });
+
+    assertEmit(socket.emit.mock.calls[1]!.arguments, "grid:state", expectedGrid);
   });
 });
